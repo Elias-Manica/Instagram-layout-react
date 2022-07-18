cf7741316eedbc3d6ab8526d8dd79878869f633a
elias <eliasmanica@poli.ufrj.br>
2022-07-18 11:56:15 -0300

    clique individual

--- src/components/ContentBody.js
+++ src/components/ContentBody.js
@@ -1,9 +1,9 @@
-import Post from "./Post";
 import Suggest from "./Suggest";
 import Story from "./Story";
 import User from "./User";
 import Bottom from "./Bottom";
 import Copyright from "./Copyright";
+import Post from "./Post";
 
 export default function ContentBody() {
   const dataUser = [
@@ -12,6 +12,42 @@ export default function ContentBody() {
       profileName: "Catana",
       urlImgUser:
         "https://elias-manica.github.io/Instagram-layout/imagens/fotos-comprovam-que-cachorros-sao-os-pets-mais-engracados-20032018161340130.jpeg",
+    },
+  ];
+
+  const post = [
+    {
+      nameUser: "meowed",
+      imgUser:
+        "https://elias-manica.github.io/Instagram-layout/imagens/meowed.png",
+      imgPost:
+        "https://elias-manica.github.io/Instagram-layout/imagens/gato-da-selfie-2.jpg",
+      imgLikedBy:
+        "https://elias-manica.github.io/Instagram-layout/imagens/responde%20ai%20logo.png",
+      likedBy: "respondeai",
+      likedOthers: "101.523",
+    },
+    {
+      nameUser: "driven",
+      imgUser:
+        "https://elias-manica.github.io/Instagram-layout/imagens/driven%20logo.png",
+      imgPost:
+        "https://elias-manica.github.io/Instagram-layout/imagens/cachorro-selfie2.jpg",
+      imgLikedBy:
+        "https://elias-manica.github.io/Instagram-layout/imagens/alien.png",
+      likedBy: "alien",
+      likedOthers: "92.321",
+    },
+    {
+      nameUser: "fluxo",
+      imgUser:
+        "https://elias-manica.github.io/Instagram-layout/imagens/fluxo%20consultoria%20logo.jpg",
+      imgPost:
+        "https://elias-manica.github.io/Instagram-layout/imagens/cachorros-fazendo-selfie_3.jpg",
+      imgLikedBy:
+        "https://elias-manica.github.io/Instagram-layout/imagens/wawawiwa.png",
+      likedBy: "wawawiwa",
+      likedOthers: "76.089",
     },
   ];
 
@@ -24,8 +60,16 @@ export default function ContentBody() {
             <ion-icon name="chevron-forward-circle"></ion-icon>
           </div>
         </div>
-
-        <Post />
+        {post.map((item) => (
+          <Post
+            nameUser={item.nameUser}
+            imgUser={item.imgUser}
+            imgPost={item.imgPost}
+            imgLikedBy={item.imgLikedBy}
+            likedBy={item.likedBy}
+            likedOthers={item.likedOthers}
+          />
+        ))}
       </div>
 
       <div class="sidebar">
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,103 +1,61 @@
 import React from "react";
 
-export default function Post() {
-  const post = [
-    {
-      nameUser: "meowed",
-      imgUser:
-        "https://elias-manica.github.io/Instagram-layout/imagens/meowed.png",
-      imgPost:
-        "https://elias-manica.github.io/Instagram-layout/imagens/gato-da-selfie-2.jpg",
-      imgLikedBy:
-        "https://elias-manica.github.io/Instagram-layout/imagens/responde%20ai%20logo.png",
-      likedBy: "respondeai",
-      likedOthers: "101.523",
-    },
-    {
-      nameUser: "driven",
-      imgUser:
-        "https://elias-manica.github.io/Instagram-layout/imagens/driven%20logo.png",
-      imgPost:
-        "https://elias-manica.github.io/Instagram-layout/imagens/cachorro-selfie2.jpg",
-      imgLikedBy:
-        "https://elias-manica.github.io/Instagram-layout/imagens/alien.png",
-      likedBy: "alien",
-      likedOthers: "92.321",
-    },
-    {
-      nameUser: "fluxo",
-      imgUser:
-        "https://elias-manica.github.io/Instagram-layout/imagens/fluxo%20consultoria%20logo.jpg",
-      imgPost:
-        "https://elias-manica.github.io/Instagram-layout/imagens/cachorros-fazendo-selfie_3.jpg",
-      imgLikedBy:
-        "https://elias-manica.github.io/Instagram-layout/imagens/wawawiwa.png",
-      likedBy: "wawawiwa",
-      likedOthers: "76.089",
-    },
-  ];
-
+export default function Post(props) {
   const [typeButtom, setTypeButtom] = React.useState("heart-outline");
 
   return (
     <div class="posts">
-      {post.map((item) => (
-        <div class="post">
-          <div class="topo">
-            <div class="usuario">
-              <img src={item.imgUser} alt="" />
-              {item.nameUser}
+      <div class="post">
+        <div class="topo">
+          <div class="usuario">
+            <img src={props.imgUser} alt="" />
+            {props.nameUser}
+          </div>
+          <div class="acoes">
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+
+        <div class="conteudo">
+          <img
+            src={props.imgPost}
+            alt=""
+            onClick={() => {
+              setTypeButtom("heart");
+            }}
+          />
+        </div>
+
+        <div class="fundo">
+          <div class="acoes">
+            <div>
+              <ion-icon
+                name={typeButtom}
+                onClick={() => {
+                  if (typeButtom === "heart") {
+                    setTypeButtom("heart-outline");
+                  } else {
+                    setTypeButtom("heart");
+                  }
+                }}
+              ></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
+            <div>
+              <ion-icon name="bookmark-outline"></ion-icon>
             </div>
           </div>
 
-          <div class="conteudo">
-            <img
-              src={item.imgPost}
-              alt=""
-              onClick={(e) => {
-                console.log(e);
-                console.log(e.target.src);
-                console.log(item.imgPost);
-                setTypeButtom("heart");
-              }}
-            />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon
-                  name={typeButtom}
-                  onClick={(e) => {
-                    console.log(e);
-                    if (typeButtom === "heart") {
-                      setTypeButtom("heart-outline");
-                    } else {
-                      setTypeButtom("heart");
-                    }
-                  }}
-                ></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src={item.imgLikedBy} alt="" />
-              <div class="texto">
-                Curtido por <strong>{item.likedBy}</strong> e{" "}
-                <strong>outras {item.likedOthers} pessoas</strong>
-              </div>
+          <div class="curtidas">
+            <img src={props.imgLikedBy} alt="" />
+            <div class="texto">
+              Curtido por <strong>{props.likedBy}</strong> e{" "}
+              <strong>outras {props.likedOthers} pessoas</strong>
             </div>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
